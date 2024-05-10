3e093c20c07cb9b8642af9aecf9783677987820a
Dang <dangvmse63447@fpt.edu.vn>
2024-05-10 08:46:20 +0700

    f

--- vite-project/src/pages/NewPage.jsx
+++ vite-project/src/pages/NewPage.jsx
@@ -1,9 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
-import Header from "../components/Header";
-import news from "../assets/news.png";
-import * as API from "../api/newsApi";
 import { useNavigate } from "react-router-dom";
+import * as API from "../api/newsApi";
+import Header from "../components/Header";
 
 const NewPage = () => {
   const [list, setList] = useState([]);
@@ -31,25 +30,50 @@ const NewPage = () => {
       <div className=" mx-auto max-w-[1500px] text-white">
         <h1 className="text-[30px] font-bold mt-4">Tin tức mới nhất</h1>
         <div className="mt-4">
-          {list?.length > 0 &&
-            list?.map((item) => (
-              <div
-                className="border-t-4 border-indigo-500 flex p-5 justify-between max-w-[1200px]"
-                key={item?.id}
-              >
-                <img src={item?.link_url || news} width={300} height={250} />
-                <div className="ml-4">
-                  <p
-                    className="text-green-500 font-bold text-[18px] cursor-pointer"
-                    onClick={() => navigate(`/news/${item?.id}`)}
-                  >
-                    {item?.title}
-                  </p>
-                  <p className="text-[#b0b0b0]">21/04/2024</p>
-                  <p className="text-[#7777]">{item?.description}</p>
-                </div>
-              </div>
-            ))}
+          <div className="grid xl:grid-cols-4 xl:gap-4 md:grid-cols-2 md:gap-6 sm:grid-cols-1 sm:gap-6">
+            {list?.length > 0 &&
+              list?.map((item) => (
+                <article
+                  key={item.id}
+                  className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out "
+                >
+                  <img
+                    src={item?.link_url}
+                    alt="Apartment Planned at 715 Main"
+                    className="h-56 w-full object-cover shadow-md rounded-t-lg"
+                  />
+                  <div className="flex-1 flex flex-col">
+                    <div className="flex-1 flex flex-col p-5 ">
+                      <h2 className="font-bold font-serif text-black ">
+                        {item?.title}
+                      </h2>
+                      <section className="mt-2 flex-1">
+                        <p className="text-xs line-clamp-3 text-blackO">
+                          {item?.description}
+                        </p>
+                      </section>
+                      <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
+                        <p>buffalorising</p>
+                        <p>
+                          <time
+                            dateTime="2024-05-09T04:10:51.000Z"
+                            title="2024-05-09T04:10:51Z"
+                          >
+                            21 hours ago
+                          </time>
+                        </p>
+                      </footer>
+                    </div>
+                    <button
+                      className="bg-orange-400 h-10 rounded-b-lg text-black font-bold hover:bg-orange-500"
+                      onClick={() => navigate(`/news/${item?.id}`)}
+                    >
+                      Read More
+                    </button>
+                  </div>
+                </article>
+              ))}
+          </div>
         </div>
       </div>
     </div>
--- vite-project/src/pages/NewPageDetail.jsx
+++ vite-project/src/pages/NewPageDetail.jsx
@@ -1,8 +1,67 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import * as API from "../api/newsApi";
+import Header from "../components/Header";
 
 const NewPageDetail = () => {
-  return <div>NewPageDetail</div>;
+  const { id } = useParams();
+  const [list, setList] = useState({});
+
+  const getList = async () => {
+    try {
+      const res = await API.getOne(id, {
+        fields: ["$all"],
+      });
+      console.log("🚀 ~ getList ~ res:", res);
+      setList(res);
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      getList();
+    }
+  }, [id]);
+  return (
+    <div>
+      {" "}
+      <Header />
+      <div className=" mx-auto max-w-[1500px] text-white">
+        <div className="mt-[50px]">
+          <div className="max-w-6xl mx-auto bg">
+            <article>
+              <section className="flex flex-col lg:flex-row px-0 lg:px-10">
+                <img
+                  src={list?.link_url}
+                  alt="article.title"
+                  className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl   object-cover object-center rounded-lg shadow-lg   "
+                />
+                <div className="px-10">
+                  <h1 className="Title px-0  pb-2">{list?.title}</h1>
+                  <div className="flex divide-x-2 space-x-4">
+                    <h2 className="font-bold">By: Buffalo Rising</h2>
+                    <h2 className="font-bold pl-4">Source: buffalorising</h2>
+                    <p className="pl-4">
+                      <time
+                        dateTime="2024-05-09T04:10:51.000Z"
+                        title="2024-05-09T04:10:51Z"
+                      >
+                        21 hours ago
+                      </time>
+                    </p>
+                  </div>
+                  <p className="pt-4">{list?.description}</p>
+                </div>
+              </section>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewPageDetail;
